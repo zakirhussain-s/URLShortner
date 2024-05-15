0cfb781326abed4c60807549910e6296f515e976
zakirhussain-s <s.zakirhussain02@gmail.com>
2024-05-15 12:55:39 +0530

    first commit

---
+++ src/main/java/com/desigining/urlShortner/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.desigining.urlShortner;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UrlShortnerApplication.class);
+	}
+
+}
